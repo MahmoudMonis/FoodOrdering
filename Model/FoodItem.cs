f7601a6b271ad1a50cddf2aa9c7190659df47bec
HadyAlzben <moqtnane@gmail.com>
2023-06-15 12:00:58 +0300

    Updated Subcategory and Item Controller

--- Model/FoodItem.cs
+++ Model/FoodItem.cs
@@ -7,16 +7,17 @@ namespace FoodOrdering.Model
 {
     public class FoodItem
     {
-    public int FoodItemId { get; set; } 
+        public int FoodItemId { get; set; }
 
-    public string Name { get; set; }
+        public string Name { get; set; }
 
-    public string Description { get; set; }
+        public string Description { get; set; }
 
-    public double Price { get; set; }
+        public double Price { get; set; }
 
-    public Subcategory subcategory { get; set; }
+        public Subcategory subcategory { get; set; }
 
-    public int IdSubcategory { get; set; }
+        public int IdSubcategory { get; set; }
+
     }
 }
--- ViewModels/FoodItemVM.cs
+++ ViewModels/FoodItemVM.cs
@@ -9,6 +9,6 @@ namespace FoodOrdering.ViewModels
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public SubCategoryVM? SubCategory {get; set;}
+        public SubCategoryVM? SubCategory { get; set; }
     }
 }
