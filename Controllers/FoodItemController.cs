bf5315c83a60704e3cbd4ca8ec199a0b888aaece
Abeer Taha <104815752+Abeertaha@users.noreply.github.com>
2023-06-15 17:39:03 +0300

    minor spelling update

--- Controllers/FoodItemController.cs
+++ Controllers/FoodItemController.cs
@@ -28,78 +28,78 @@ namespace FoodOrdering.Controller
         {
             if (subcategory != null && name != null)
             {
-                return _Context.fooditem.Where(x => x.IdSubcategory == subcategory && x.Name == name).ToList();
+                return _Context.foodItem.Where(x => x.IdSubcategory == subcategory && x.Name == name).ToList();
 
             }
             else if (subcategory != null)
             {
-                return _Context.fooditem.Where(x => x.IdSubcategory == subcategory).ToList();
+                return _Context.foodItem.Where(x => x.IdSubcategory == subcategory).ToList();
 
             }
             else if (name != null)
             {
-                return _Context.fooditem.Where(x => x.Name == name).ToList();
+                return _Context.foodItem.Where(x => x.Name == name).ToList();
 
             }
             else
             {
-                return _Context.fooditem.ToList();
+                return _Context.foodItem.ToList();
             }
 
         }
 
 
         [HttpPost]
-        public string UpdateFoodItem(CreateFoodItemVM Fooditem)
+        public string UpdateFoodItem(CreateFoodItemVM FoodItem)
         {
 
-            if (checkitemexisiens(Fooditem.Name))
+            if (checkitemexisted(FoodItem.Name))
             {
-                FoodItem item = _Context.fooditem.FirstOrDefault(x => x.Name == Fooditem.Name);
+                FoodItem item = _Context.foodItem.FirstOrDefault(x => x.Name == FoodItem.Name);
                 bool changes = false;
-                if (item.Name != Fooditem.Name) { item.Name = Fooditem.Name; _Context.SaveChanges(); changes = true; } else {/*Nothing To Update */}
-                if (item.Description != Fooditem.Description) { item.Description = Fooditem.Description; _Context.SaveChanges(); changes = true; } else {/*Nothing To Update */}
-                if (item.Price != Fooditem.Price) { item.Price = Fooditem.Price; _Context.SaveChanges(); changes = true; } else {/*Nothing To Update */}
-                if (item.IdSubcategory != Fooditem.SubCategoryId) { item.IdSubcategory = Fooditem.SubCategoryId; _Context.SaveChanges(); changes = true; } else {/*Nothing To Update */}
+                if (item.Name != FoodItem.Name) { item.Name = FoodItem.Name; _Context.SaveChanges(); changes = true; } else {/*Nothing To Update */}
+                if (item.Description != FoodItem.Description) { item.Description = FoodItem.Description; _Context.SaveChanges(); changes = true; } else {/*Nothing To Update */}
+                if (item.Price != FoodItem.Price) { item.Price = FoodItem.Price; _Context.SaveChanges(); changes = true; } else {/*Nothing To Update */}
+                if (item.IdSubcategory != FoodItem.SubCategoryId) { item.IdSubcategory = FoodItem.SubCategoryId; _Context.SaveChanges(); changes = true; } else {/*Nothing To Update */}
 
 
                 if (changes == true) { return "Item Has Been Updated"; } else { return "Item Found But nothing has been updated  no new changes "; }
             }
             else
             {
-                return "There is no Such an item Like " + Fooditem.Name;
+                return "There is no Such an item Like " + FoodItem.Name;
             }
 
         }
 
 
         [HttpPost]
-        public string AddFoodItem(CreateFoodItemVM Fooditem)
+        public string AddFoodItem(CreateFoodItemVM FoodItem)
         {
-            if (checkitemexisiens(Fooditem.Name))
+            if (checkitemexisted(FoodItem.Name))
             {
                 //Add Update Function 
-                return "You cant add already exisited Item";
+                return "You cant add already existed Item";
             }
             else
             {
                 _Context.Add(new FoodItem
                 {
-                    Name = Fooditem.Name,
-                    Description = Fooditem.Description,
-                    Price = Fooditem.Price,
-                    IdSubcategory = Fooditem.SubCategoryId
+                    Name = FoodItem.Name,
+                    Description = FoodItem.Description,
+                    Price = FoodItem.Price,
+                    IdSubcategory = FoodItem.SubCategoryId
                 });
                 _Context.SaveChanges();
 
-                return JsonSerializer.Serialize(Fooditem);
+                return JsonSerializer.Serialize(FoodItem);
             }
 
         }
 
-        private bool checkitemexisiens(string Name)
+        private bool checkitemexisted(string Name)
         {
-            FoodItem item = _Context.fooditem.FirstOrDefault(x => x.Name == Name);
+            FoodItem item = _Context.foodItem.FirstOrDefault(x => x.Name == Name);
             if (item != null && item.Name != null) { return true; } else { return false; }
         }
 
