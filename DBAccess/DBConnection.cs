59a18fab3aaf67da4cfdbfa39fe6bb2b4ab8503d
Abeer Taha <104815752+Abeertaha@users.noreply.github.com>
2023-06-15 17:31:16 +0300

    update

--- DBAccess/DBConnection.cs
+++ DBAccess/DBConnection.cs
@@ -13,7 +13,7 @@ namespace FoodOrdering.DBAccess
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("host=localhost;port=5432;dbname=postgres;user=postgres;password=123456;sslmode=prefer;connect_timeout=10");
 
-        public DbSet<FoodItem>? fooditem { get; set; }
+        public DbSet<FoodItem>? foodItem { get; set; }
         public DbSet<Basket1>? Basket { get; set; }
         public DbSet<Order>? order { get; set; }
         public DbSet<OrderItems>? orderItems { get; set; }
